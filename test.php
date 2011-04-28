58a86407f1075a861b52325d580433a7b618afff
Gaffey <tuzik.gaffey@gmail.com>
2011-04-28 14:42:30 +0800

    fix

--- test.php
+++ test.php
@@ -9,12 +9,11 @@ $config = array('host' => '120.132.144.174',
 		  'password' => 'aspire.de');
 
 $db = new DBConnection($config,$debug = true, $profile = true);
+$db->setAlive(true);
 
-var_dump($db->query('select * from user_confirm'));
-
-var_dump($db->_pdo);
-
-$db2 = new DBConnection($config);
-var_dump($db->_pdo);
+var_dump($db->exec(" insert into user_confirm (`user_id` , `type`,`code`,`status`,`time_create`)values (12112,'mobile','111','invalid','221111')"));
+var_dump($db->exec(" insert into user_confirm (`user_id` , `type`,`code`,`status`,`time_create`)values (1112112,    'mobile','111','invalid','221111')"));
+var_dump($db->exec(" insert into user_confirm (`user_id` , `type`,`code`,`status`,`time_create`)values (12112,    'mobile','111','invalid','22111199')"));
 
 
+var_dump($db->query("select * from user_confirm"));
